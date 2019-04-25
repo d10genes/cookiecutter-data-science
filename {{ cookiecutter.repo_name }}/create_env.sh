9c0c6d35c5e8c93460ac00d9ac505a1abf2b9d9d
wcbeard <wcbeard10@gmail.com>
2019-04-24 21:40:09 -0400

    tox and friends

---
+++ {{ cookiecutter.repo_name }}/create_env.sh
@@ -0,0 +1,7 @@
+set -euo pipefail
+
+conda env create -f environment.yml
+
+source activate {{cookiecutter.repo_name}}
+
+jupyter labextension install jupyterlab_vim @jupyterlab/toc @ryantam626/jupyterlab_code_formatter
