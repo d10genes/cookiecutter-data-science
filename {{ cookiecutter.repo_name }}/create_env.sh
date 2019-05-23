15d4654ae13b09dba8d85524e6e0c074a78866ff
wcbeard <wcbeard10@gmail.com>
2019-05-23 15:54:03 -0400

    add jupytext to create script

--- {{ cookiecutter.repo_name }}/create_env.sh
+++ {{ cookiecutter.repo_name }}/create_env.sh
@@ -5,3 +5,7 @@ conda env create -f environment.yml
 source activate {{cookiecutter.repo_name}}
 
 jupyter labextension install jupyterlab_vim @jupyterlab/toc @ryantam626/jupyterlab_code_formatter
+
+cd notebooks
+jupytext --to ipynb templ.py
+cd ..
