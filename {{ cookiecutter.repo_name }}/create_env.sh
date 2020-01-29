28ef3bc474ec6c22da74dc25843add0fd411f0a2
wcbeard <wcbeard10@gmail.com>
2020-01-29 11:53:37 +0100

    build jupyter with name

--- {{ cookiecutter.repo_name }}/create_env.sh
+++ {{ cookiecutter.repo_name }}/create_env.sh
@@ -5,6 +5,7 @@ conda env create -f environment.yml
 source activate {{cookiecutter.repo_name}}
 
 jupyter labextension install jupyterlab_vim @jupyterlab/toc @ryantam626/jupyterlab_code_formatter
+jupyter lab build --name='{{cookiecutter.repo_name}}'
 
 cd notebooks
 jupytext --to ipynb templ.py
